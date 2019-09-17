5024a15ad8ed4c755a6a51f3fa7155b3330c4d49
Ron Rise <rrise@fuseos.net>
2019-09-17 15:17:18 -0400

    center message

--- src/js/Pages/Index.vue
+++ src/js/Pages/Index.vue
@@ -1,11 +1,17 @@
 <template>
-  <v-content>
-    <v-container>
-      <v-flex sm6>
-        <SuccessMessage />
-      </v-flex>
-    </v-container>
-  </v-content>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-row
+          justify="center"
+        >
+          <v-flex md6>
+            <SuccessMessage />
+          </v-flex>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 import SuccessMessage from '../Components/SuccessMessage.vue'
