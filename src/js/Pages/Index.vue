cd742b4702c3a0f821bc4fc03e2891579a9b3062
Ron Rise <rrise@fuseos.net>
2019-09-17 13:25:31 -0400

    Company list example

--- src/js/Pages/Index.vue
+++ src/js/Pages/Index.vue
@@ -3,18 +3,15 @@
     <v-container>
       <v-flex sm6>
         <SuccessMessage />
-        <CompanyList  />
       </v-flex>
     </v-container>
   </v-content>
 </template>
 <script>
 import SuccessMessage from '../Components/SuccessMessage.vue'
-import CompanyList from '../Components/CompanyList.vue'
 
 export default {
   components: {
-    CompanyList,
     SuccessMessage
   }
 }
